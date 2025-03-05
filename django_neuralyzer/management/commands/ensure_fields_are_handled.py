2c07f48b03272237242f7ff97062f7930a1fbac0
Samuël WEBER <s.weber@webu.coop>
2025-03-05 17:51:58 +0100

    feat(management): better logging of missing neuralized fields and extra fields in neuralyzer

--- django_neuralyzer/management/commands/ensure_fields_are_handled.py
+++ django_neuralyzer/management/commands/ensure_fields_are_handled.py
@@ -1,6 +1,8 @@
 import logging
 
 from django.core.management.base import BaseCommand
+from django.db.models.fields.related import ManyToManyField
+from django.db.models.fields.related import OneToOneField
 
 from django_neuralyzer.base import BaseNeuralyzer
 from django_neuralyzer.utils import get_app_submodules
@@ -26,12 +28,26 @@ class Command(BaseCommand):
         errors = []
 
         for klass in anon_classes:
+            print(f"Neuralyzer: {klass}")
             model = klass.Meta.model
             anon_fields = klass()._get_class_attributes()
-            for field in model._meta.fields:
+            model_fields = model._meta.fields
+            model_fields_names = [field.name for field in model_fields]
+            for field in model_fields:
+                if isinstance(field, (ManyToManyField, OneToOneField)):
+                    continue
+
                 if field.name not in anon_fields:
                     errors.append(
                         f"Neuralyzer {klass.__name__} is missing field {field.name} for model {model.__name__}"
                     )
+            for field in anon_fields:
+                if field not in model_fields_names:
+                    errors.append(
+                        f"Neuralyzer {klass.__name__} has extra field {field} for model {model.__name__}"
+                    )
+
         if errors:
             logger.error("Following models have not been fully handled: \n" + "\n".join(errors))
+        else:
+            print("All models neuralyzed include all fields!")
